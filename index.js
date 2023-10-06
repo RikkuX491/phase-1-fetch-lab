f9daa3dd8bd83e0154e2fbc91ee37d2bc228546e
Ricardo Guerra <RikkuG491@gmail.com>
2023-10-06 11:40:11 -0400

    Added solution code with extra content

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(booksData => {
+    renderBooks(booksData)
+    
+    // Extra code
+    renderCharactersForFirstBook(booksData[0].characters)
+  })
 }
 
 function renderBooks(books) {
@@ -15,3 +22,19 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+// Extra function
+function renderCharactersForFirstBook(characters){
+  const div = document.createElement('div')
+  
+  const title = document.createElement('h1')
+  title.textContent = "Characters Section:"
+  div.appendChild(title)
+
+  characters.forEach(character => {
+    const h1 = document.createElement('h1')
+    h1.textContent = character
+    div.appendChild(h1)
+  })
+  document.body.appendChild(div)
+}
